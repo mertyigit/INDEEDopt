862f3cd43bea381dc5fedb7f02da289914cd0afa
Mert Sengul <mertyigitsengul@gmail.com>
2021-02-11 09:03:31 -0500

    Add files via upload
    Updated due to recent Tensorflow update.

--- error_output.py
+++ error_output.py
@@ -25,3 +25,31 @@ def error_output(number_of_parameters):
             file.close()
     except IOError:
         pass
+
+def error_output_bf(number_of_parameters, parameters_file_name):
+    
+    """Write the optimized error values into the params file near the corresponding parameter identicator"""
+    
+    try:
+        
+        file2 = open(parameters_file_name, "a+")
+        #file2.seek(0)
+        #position2 = file2.tell()
+        
+        file = open("fort.13","r")        
+        file.seek(0)
+        position = file.tell()
+        
+        try:
+            
+            file.seek(0)
+            error = file.read(12)
+            
+            #file2.seek(20)
+            file2.write("\n TOTAL ERROR: " + error.rjust(18))
+        
+        finally:
+            file.close()
+    except IOError:
+        pass
+    return error
